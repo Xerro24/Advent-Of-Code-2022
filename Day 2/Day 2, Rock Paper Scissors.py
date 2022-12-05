3a49745d7029c29b1ef8a0e9bcc11e9a0384693f
Xerro <xerro24@gmail.com>
2022-12-05 13:04:44 -0800

    Finished Day 2 part 2

--- Day 2/Day 2, Rock Paper Scissors.py
+++ Day 2/Day 2, Rock Paper Scissors.py
@@ -5,17 +5,31 @@ score = 0
 with open('Strategy Guide.txt', 'r') as file:
     for line in file:
         Opponent_move = line.split(" ")[0]
-        Player_move = line.split(" ")[1]
-        Player_move = Player_move.split("\n")[0]
-        print(Opponent_move,Player_move)
-        score += ord(Player_move)-87
-        Player_move = ord(Player_move)-87
+        End_State = line.split(" ")[1]
+        End_State = End_State.split("\n")[0]
+        print(Opponent_move,End_State)
+        
+        End_State = ord(End_State)-87
         Opponent_move = ord(Opponent_move)-64
-        #print(Opponent_move,Player_move)
-        if (Player_move - 1 == Opponent_move or Player_move + 2 == Opponent_move):
-            score += 6
-        elif (Player_move == Opponent_move):
+        if End_State == 2:
             score += 3
+        elif End_State == 3:
+            score += 6
+        
+
+        #print(Opponent_move,Player_move)
+        if (End_State == 1):
+            if (Opponent_move != 1):
+                score += Opponent_move-1
+            else:
+                score += 3
+        elif (End_State == 2):
+            score += Opponent_move
+        else:
+            if (Opponent_move != 3):
+                score += Opponent_move+1
+            else:
+                score += 1
         print(score)
 
 
