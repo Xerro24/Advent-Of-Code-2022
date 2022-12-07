49c6dc38e352f7cb445af2c0e1e4b0dc46cdeead
Xerro <xerro24@gmail.com>
2022-12-07 13:25:51 -0800

    Finished Day 3 part 2

--- Day 3/Day 3, Rucksack Reorganization.py
+++ Day 3/Day 3, Rucksack Reorganization.py
@@ -2,8 +2,28 @@ print('\n\n\n')
 
 
 priority_sum = 0
+grouping = 0
+Compartments = [0,0,0]
 with open('Rucksack.txt', 'r') as file:
     for line in file:
+        Compartments[grouping] = line
+        grouping += 1
+        if (grouping == 3):
+            grouping = 0
+            for i,x in enumerate(Compartments):
+                Compartments[i] = set(x)
+                Compartments[i].discard('\n')
+            Common_item =  Compartments[0] & Compartments[1] & Compartments[2]
+            for i in Common_item:
+                Common_item = i
+            print(Common_item)
+            if (ord(Common_item)-96 < 0):
+                priority_sum += ord(Common_item)-38
+            else:
+                priority_sum += ord(Common_item)-96
+            print(priority_sum)
+
+        '''
         Compartment_1 = line[0:len(line)//2]
         Compartment_2 = line[len(line)//2:len(line)]
         Compartment_1 = set(Compartment_1)
@@ -21,6 +41,7 @@ with open('Rucksack.txt', 'r') as file:
         print(priority_sum)
 
         #print(Compartment_1 + "    " + Compartment_2)
+        '''
 
 
 
