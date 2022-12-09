cafa71d3f1ac8407501cad5a64f979475f1330e0
Xerro <xerro24@gmail.com>
2022-12-09 11:05:55 -0800

    Day 4 part 2 done

--- Day 4/Day 4, Camp Cleanup.py
+++ Day 4/Day 4, Camp Cleanup.py
@@ -3,10 +3,25 @@
 print('\n\n\n')
 
 
-
-with open('Elf Task Assignment - Copy.txt', 'r') as file:
+num_of_tasks = 0
+with open('Elf Task Assignment.txt', 'r') as file:
     for line in file:
-        line.split()
+        temp = line.split(',')
+        Task_range = []
+        for i in temp:
+            Task_range.append(i.split('-'))
+        Task_range[1][1] = Task_range[1][1].split("\n")[0]
+        for i,x in enumerate(Task_range):
+            for j in range(2):
+                Task_range[i][j] = int(x[j])
+        
+
+        if ((Task_range[0][0] >= Task_range[1][0] and Task_range[0][0] <= Task_range[1][1]) or (Task_range[1][0] >= Task_range[0][0] and Task_range[1][0] <= Task_range[0][1])) or ((Task_range[0][0] <= Task_range[1][0] and Task_range[0][1] >= Task_range[1][1]) or (Task_range[1][0] <= Task_range[0][0] and Task_range[1][1] >= Task_range[0][1])):
+            num_of_tasks += 1
+        #if (Task_range[0][0] <= Task_range[1][0] and Task_range[0][1] >= Task_range[1][1]) or (Task_range[1][0] <= Task_range[0][0] and Task_range[1][1] >= Task_range[0][1]):
+        
+        print(Task_range)
+        print(num_of_tasks)
         
 
 
